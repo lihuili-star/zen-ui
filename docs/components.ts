16f91fc5372c0c46ce005faaea8c15d18588f64d
ZBADW <291412082@qq.com>
2023-02-04 12:44:42 +0800

    文档bug修复

--- docs/components.ts
+++ docs/components.ts
@@ -1,4 +1,3 @@
 export const components = [
-  { text: 'Foo 组件示例', link: '/components/foo' },,
-  { text: 'Xxx xxx', link: '/components/xxx' }
+  { text: 'Foo 组件示例', link: '/components/foo' }
 ] // end
